309563d6fcd961f1319bcb6521fe904edd1f72ef
safya0 <safyahosam0523064.gmail.com>
2024-11-18 06:31:03 +0200

    Add hello Fatme

---
+++ Managment/Managment/Login.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Managment
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var corect = "Admin123";
+                Management management = new Management();
+                View view = new View();
+                Userr userr = new Userr();
+                var pass = Passwordtxt.Text;
+                var em = Emailtxt;
+
+                //if (Passwordtxt.Text == corect == userr.UserPassword)
+
+                
+                if (Passwordtxt.Text == corect)
+                {
+                        NavigationService.Navigate(management);
+                        MessageBox.Show("Login Successfuly");
+                }
+                    if (userr.UserPassword == Passwordtxt.Text)
+                    {
+                        NavigationService.Navigate(view);
+                        MessageBox.Show("Login Successfuly");
+
+                    }
+                
+                MessageBox.Show("Error, Please Try Agign");
+
+            }
+            catch
+            {
+                MessageBox.Show("Error, Please Try Agign");
+            }
+        }
+    }
+}
---
+++ Managment/Managment/Management.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Managment
+{
+    /// <summary>
+    /// Interaction logic for Management.xaml
+    /// </summary>
+    public partial class Management : Page
+    {
+        MagagmentEntities DB = new MagagmentEntities();
+        public Management()
+        {
+            InitializeComponent();
+            DG.ItemsSource=DB.Tasks.ToList();
+        }
+        void Clear() 
+        {
+            taskxid.Text = Titletxt.Text = destxt.Text = "";
+
+
+        }
+        void Refresh()
+        {
+            DG.ItemsSource = DB.Tasks.ToList();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Task task = new Task();
+                task.TaskId = int.Parse(taskxid.Text);
+                task.Title = Titletxt.Text;
+                task.DescriptionTask = destxt.Text;
+                compo.ItemsSource = task.StatusTask;
+                DB.Tasks.Add(task);
+                DB.SaveChanges();
+                Refresh();
+                Clear();
+                MessageBox.Show("Adding Successfully");
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Task task = new Task();
+                task.TaskId = int.Parse(taskxid.Text);
+                task.Title = Titletxt.Text;
+                task.DescriptionTask = destxt.Text;
+                DB.Tasks.AddOrUpdate(task);
+                DB.SaveChanges();
+                Refresh();
+                Clear();
+                MessageBox.Show("Editing Successfully");
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Task task = new Task();
+                var id = int.Parse(taskxid.Text);
+                DB.Tasks.Remove(DB.Tasks.FirstOrDefault(T => T.TaskId == id));
+                DB.SaveChanges();
+                Refresh();
+                Clear();
+                MessageBox.Show("DELETE Successfully");
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+
+            }
+        }
+
+        private void compo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DG.ItemsSource=compo.Items;
+        }
+    }
+}
